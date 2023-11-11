b6c675613f3c420f2f7724149f4f400e5a79990d
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2023-11-11 12:05:25 +0100

    feat(core): add client static build directory

--- api/src/Controller/LogoutController.php
+++ api/src/Controller/LogoutController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[AsController]
 class LogoutController extends AbstractController
 {
-    #[Route('/logout', name: 'app_logout', methods: ['POST'])]
+    #[Route('/api/logout', name: 'app_logout', methods: ['POST'])]
     public function __invoke(): Response
     {
         return new Response('', Response::HTTP_NO_CONTENT);
