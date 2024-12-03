d52d6c84ef876fa71d3593d4863015d6698110fa
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2024-12-03 17:57:05 +0100

    feat(core): add base microstratigraphic unit resource

---
+++ api/src/Entity/Data/MicroStratigraphicUnit.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity\Data;
+
+class MicroStratigraphicUnit
+{
+    public readonly int $id;
+    public Sample $sample;
+    public StratigraphicUnit $stratigraphicUnit;
+    public ?int $number;
+    public string $depositType;
+
+    public ?string $keyAttributes;
+
+    public int $inclusionsGeology = 0;
+    public int $inclusionsBuildingMaterials = 0;
+    public int $inclusionsDomesticRefuse = 0;
+    public int $inclusionsOrganicRefuse = 0;
+
+    public string $colourPpl;
+    public string $colourXpl;
+    public string $colourOil;
+
+    public bool $lenticularPlateyPeds = false;
+    public bool $crumbsOrGranules = false;
+    public bool $saBlockyPeds = false;
+    public bool $cracks = false;
+    public bool $infillings = false;
+
+    public int $mesofaunaRootBioturbation = 0;
+    public int $earthwormInternalChamber = 0;
+    public int $organicOrganoMineral = 0;
+    public int $earthwormGranule = 0;
+
+    public ?string $interpretation;
+}
